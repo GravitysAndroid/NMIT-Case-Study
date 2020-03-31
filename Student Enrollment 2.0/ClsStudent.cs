670ca121991cc70ae6a0fcd3059bfc5ee41d1f46
Hayden <55818671+GravitysAndroid@users.noreply.github.com>
2020-04-01 11:52:49 +1300

    Add project files.

---
+++ Student Enrollment 2.0/ClsStudent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Student_Enrollment_2._0
+{
+    class ClsStudent
+    {
+        private string _ID;
+        private string _Name;
+        private DateTime _DOB = DateTime.Today;
+        private decimal _Balance;
+
+        public override string ToString()
+        {
+            return _ID + "\n" + _Name;
+        }
+
+        public string ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        public DateTime DOB
+        {
+            get { return _DOB; }
+            set { _DOB = value; }
+        }
+
+        public decimal Balance
+        {
+            get { return _Balance; }
+            set { _Balance = value; }
+        }
+    }
+}
